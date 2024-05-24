a3a9f6763465e9604f63dc538baada7522784316
Grejs Madsen <104923237+GrejsM@users.noreply.github.com>
2024-05-24 08:59:06 +0200

    init

---
+++ user-service/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://mongo:27017/userdb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/register', async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+});
+
+app.post('/login', async (req, res) => {
+    const user = await User.findOne({ username: req.body.username, password: req.body.password });
+    if (!user) {
+        return res.status(401).send('Invalid credentials');
+    }
+    res.send(user);
+});
+
+app.listen(3000, () => {
+    console.log('User service listening on port 3000');
+});
