77ec0dd7c311f2f98d5f93ac57b4fb5dbe2f5b81
Grejs Madsen <104923237+GrejsM@users.noreply.github.com>
2024-05-24 16:03:36 +0200

    Update build_and_push.sh with correct paths

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd services || exit 1
+
 services="user-service chat-service message-service auth-service"
 for service in $services; do
     docker build -t grejs420/$service:latest ./$service
