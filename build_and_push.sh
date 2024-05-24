a324d07d143048bce78209a5bf8fac4d242755b4
Grejs Madsen <104923237+GrejsM@users.noreply.github.com>
2024-05-24 16:00:46 +0200

    Fix syntax error in build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-services=("user-service" "chat-service" "message-service" "auth-service")
-for service in "${services[@]}"; do
+services="user-service chat-service message-service auth-service"
+for service in $services; do
     docker build -t grejs420/$service:latest ./$service
     docker push grejs420/$service:latest
 done
