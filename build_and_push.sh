68783bc5a30a23feba1abb49ba65967215a8e1b8
Grejs Madsen <104923237+GrejsM@users.noreply.github.com>
2024-05-25 14:11:21 +0200

    trying to fix services

--- build_and_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd backend || exit 1
-
-services="user-service chat-service message-service auth-service"
-for service in $services; do
-    docker build -t grejs420/$service:latest ./$service
-    docker push grejs420/$service:latest
-done
