1df588a0bfd0c486b4cc2481e40371b95a051001
@@au695345 <202109831@post.au.dk>
2024-05-28 19:08:17 +0200

    ny frontend for se om det fikser fejlene

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,17 @@ import Login from './components/Login';
 import Users from './components/Users';
 import Chats from './components/Chats';
 import Messages from './components/Messages';
-import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/chats" element={<Chats />} />
-        <Route path="/chats/:id/messages" element={<Messages />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/chats/:id/messages" element={<Messages />} />
         </Routes>
       </div>
     </Router>
