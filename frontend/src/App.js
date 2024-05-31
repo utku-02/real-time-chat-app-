670f3cee0dcdea85f5633ebf863d8a6e65fa731a
@@au695345 <202109831@post.au.dk>
2024-05-31 11:31:13 +0200

    få ændringer: tilføjet namspace i ingress og path i nginx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import Messages from './components/Messages';
 
 function App() {
   return (
-    <Router >
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
