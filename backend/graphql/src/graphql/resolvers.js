085c8ab57bd3dcb15b67f37124be600cb40fd7a0
Grejs Madsen <104923237+GrejsM@users.noreply.github.com>
2024-06-02 18:52:30 +0200

    trying to fix graphql

--- backend/graphql/src/graphql/resolvers.js
+++ backend/graphql/src/graphql/resolvers.js
@@ -1,56 +1,52 @@
-(async () => {
-    const { GraphQLClient } = await import('graphql-request');
+import { GraphQLClient } from 'graphql-request';
 
-    const client = new GraphQLClient(process.env.GRAPHQL_URL);
+const client = new GraphQLClient(process.env.GRAPHQL_URL);
 
-    const resolvers = {
-        Query: {
-            user: async (_, { id }) => {
-                const query = `query ($id: ID!) { user(id: $id) { id email username } }`;
-                const variables = { id };
-                const data = await client.request(query, variables);
-                return data.user;
-            },
-            chatRoom: async (_, { id }) => {
-                const query = `query ($id: ID!) { chatRoom(id: $id) { id name members { id email username } } }`;
-                const variables = { id };
-                const data = await client.request(query, variables);
-                return data.chatRoom;
-            },
-            message: async (_, { id }) => {
-                const query = `query ($id: ID!) { message(id: $id) { id content sender { id email username } chatRoom { id name } } }`;
-                const variables = { id };
-                const data = await client.request(query, variables);
-                return data.message;
-            }
+export const resolvers = {
+    Query: {
+        user: async (_, { id }) => {
+            const query = `query ($id: ID!) { user(id: $id) { id email username } }`;
+            const variables = { id };
+            const data = await client.request(query, variables);
+            return data.user;
         },
-        Mutation: {
-            createUser: async (_, { input }) => {
-                const mutation = `mutation ($input: CreateUserInput!) { createUser(input: $input) { id email username } }`;
-                const variables = { input };
-                const data = await client.request(mutation, variables);
-                return data.createUser;
-            },
-            updateUser: async (_, { id, input }) => {
-                const mutation = `mutation ($id: ID!, $input: UpdateUserInput!) { updateUser(id: $id, input: $input) { id email username } }`;
-                const variables = { id, input };
-                const data = await client.request(mutation, variables);
-                return data.updateUser;
-            },
-            createChatRoom: async (_, { input }) => {
-                const mutation = `mutation ($input: CreateChatRoomInput!) { createChatRoom(input: $input) { id name members { id email username } } }`;
-                const variables = { input };
-                const data = await client.request(mutation, variables);
-                return data.createChatRoom;
-            },
-            createMessage: async (_, { input }) => {
-                const mutation = `mutation ($input: CreateMessageInput!) { createMessage(input: $input) { id content sender { id email username } chatRoom { id name } } }`;
-                const variables = { input };
-                const data = await client.request(mutation, variables);
-                return data.createMessage;
-            }
+        chatRoom: async (_, { id }) => {
+            const query = `query ($id: ID!) { chatRoom(id: $id) { id name members { id email username } } }`;
+            const variables = { id };
+            const data = await client.request(query, variables);
+            return data.chatRoom;
+        },
+        message: async (_, { id }) => {
+            const query = `query ($id: ID!) { message(id: $id) { id content sender { id email username } chatRoom { id name } } }`;
+            const variables = { id };
+            const data = await client.request(query, variables);
+            return data.message;
         }
-    };
-})();
-
-module.exports = resolvers;
+    },
+    Mutation: {
+        createUser: async (_, { input }) => {
+            const mutation = `mutation ($input: CreateUserInput!) { createUser(input: $input) { id email username } }`;
+            const variables = { input };
+            const data = await client.request(mutation, variables);
+            return data.createUser;
+        },
+        updateUser: async (_, { id, input }) => {
+            const mutation = `mutation ($id: ID!, $input: UpdateUserInput!) { updateUser(id: $id, input: $input) { id email username } }`;
+            const variables = { id, input };
+            const data = await client.request(mutation, variables);
+            return data.updateUser;
+        },
+        createChatRoom: async (_, { input }) => {
+            const mutation = `mutation ($input: CreateChatRoomInput!) { createChatRoom(input: $input) { id name members { id email username } } }`;
+            const variables = { input };
+            const data = await client.request(mutation, variables);
+            return data.createChatRoom;
+        },
+        createMessage: async (_, { input }) => {
+            const mutation = `mutation ($input: CreateMessageInput!) { createMessage(input: $input) { id content sender { id email username } chatRoom { id name } } }`;
+            const variables = { input };
+            const data = await client.request(mutation, variables);
+            return data.createMessage;
+        }
+    }
+};
