10d315ca75da5ba7b36863c722d964b369c3f206
Grejs Madsen <104923237+GrejsM@users.noreply.github.com>
2024-06-03 12:12:29 +0200

    fixed swagger

--- backend/api-gateway/src/index.js
+++ backend/api-gateway/src/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiGatewayRoutes = require('./routes/apiGatewayRoutes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./swaggerDef');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/api', apiGatewayRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
   console.log(`API Gateway running on port ${port}`);
-  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
 });
---
+++ backend/api-gateway/src/swagger.js
@@ -0,0 +1,27 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Real-Time Chat App API',
+            version: '1.0.0',
+            description: 'API documentation for the Real-Time Chat App',
+        },
+        servers: [
+            {
+                url: 'http://swwao.courses.orbit.au.dk/grp-6',
+                description: 'API Gateway',
+            },
+        ],
+    },
+    apis: ['./src/routes/apiGatewayRoutes.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = {
+    swaggerUi,
+    specs,
+};
